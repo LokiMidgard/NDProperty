7698a0e9b7db875139076fcaf94883e3192948cb
Patrick Kranz <patrick-kranz@live.de>
2017-06-15 11:14:59 +0200

    Reorganizing

---
+++ NDProperty.Analyzer/NDProperty.Analyzer.Dependencys/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDProperty.Analyzer.Dependencys
+{
+    // This Project is only to download dependency via nuget so NDPropety.Analyzer can pack them.
+}
